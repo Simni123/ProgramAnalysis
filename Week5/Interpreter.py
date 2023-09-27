32d9038da2aa94b362a989323812b4e7d2673854
jonatan lund <jonathanslund1@gmail.com>
2023-09-27 14:37:43 +0200

    Week5 update

--- Week5/Interpreter.py
+++ Week5/Interpreter.py
@@ -1,16 +1,3 @@
-class Interpreter:
-    def __init__(self):
-        self.memory = {}
-        self.stack = []
-    
-    def run(self, method):
-        self.stack.append(method)
-        while len(self.stack) != 0:
-            self.step
-
-    def step(self):
-        (localVars, opStack, pc) = self.stack[-1]
-
 #Finding all the json files
 from pathlib import Path
 import json
@@ -25,6 +12,8 @@ for f in folder.glob("**/*.json"):
 #Finding all case methods
 mehtods = {}
 for jfile in jsons.values():
+    if jfile["name"] != "eu/bogoe/dtu/exceptional/Arithmetics": #working with only arithmatic
+        continue
     for method in jfile["methods"]:
         if method["name"] != "<init>" and method["name"] != "<clinit>":
             newKey = {method["name"] : method}
@@ -41,8 +30,41 @@ def print_bytecode(caseName):
         print(codePices)
 
 #Building method program
+class Interpreter:
+    def __init__(self, log):
+        self.states = {}
+        self.log = log
+    
+    def run(self, caseName, param, k):
+        mstack={}
+        mstack[0] = (param,[],(caseName,0))
+        #mstack[1] = (param,[1],(caseName,0))
+        self.states[0] = mstack
+        step = 0
+        while k-step >= 0:
+            stepStates = self.states[step]
+            mstack={}
+            stackIndex = 0
+            for index in stepStates:
+                self.log("->", stepStates[index], end="")
+                (localVars, opStack, (cn_,i)) = stepStates[index]
+                byte = find_method(caseName)["code"]["bytecode"][i]
+                
+                self.log("  fail    ", byte)
+                stackIndex += 1
+            step += 1
+            return
+
+    def step(self):
+        (localVars, opStack, pc) = self.stack[-1]
+
+'''
 def interp(caseName, log, param):
     state = {}
+    abs_param = sign_astraction(param, log)
+    print(abs_param)
+    state[0] = (abs_param, [])
+    
     mstack = [(param,[],(caseName, 0))]
     while True:
         log("->", mstack, end="")
@@ -50,12 +72,32 @@ def interp(caseName, log, param):
         byte = find_method(caseName)["code"]["bytecode"][i]
         log(" fail", byte)
         return None
+        
+    return None
+'''
+    
+def sign_astraction(param, log):
+    abstract_param = []
+    for p in param:
+        if type(param[p]) == int:
+            if param[p] < 0:
+                abstract_param[p] = {'-'}
+            elif param[p] > 0:
+                abstract_param[p] = {'+'}
+            else:
+                abstract_param[p] = {'0'}
+        else:
+            log("Abstract type not yet supported", param[p], type(param[p]))
+            return None
+    return abstract_param
 
-print_bytecode("alwaysThrows1")
-
-#setting arguments
-#memory = {}
-#memory[0] = [5]
+#setting arguments (need to be a list - make a list of a long length)
+#memory = [5,-3]
+#memory[0] = 5
+#memory[1] = -3
 #Running the interpurator
-#s = interp("firstSafe", print, memory)
+interpret = Interpreter(print)
+interpret.run("alwaysThrows1", [], 1)
+
+#print_bytecode("alwaysThrows1")
 #print(s)
